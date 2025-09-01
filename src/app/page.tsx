74edf38c3baea868828c932f0bac4661bfa74bf7
Ostap Prodanyuk <105784216+OstapoKapo@users.noreply.github.com>
2025-09-01 07:36:47 +0300

    Auth (#14)
    * feat: create connect with deploy server for vercel
    
    * feat: create connect with deploy server for vercel
    
    * feat: create connect with deploy server for vercel
    
    * feat: craete connect to server API, closes #12
    
    * feature: add secure with session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 import AuthForm from "./components/ui/authForm/authForm";
 import { Toaster } from 'react-hot-toast';
+import { redirect } from "next/navigation";
+import { checkAuthSSR } from "@/api/auth";
+import { headers } from "next/headers";
 
 
-export default function Home() {
+const HomePage = async () => {
+
+  const headersList = await headers();
+  const cookieHeader = headersList.get('cookie') ?? '';
+  const csrfToken = headersList.get('X-CSRF-Token') ?? '';
+  const authResult = { user: null, error: null };
+  try {
+    authResult.user = await checkAuthSSR(cookieHeader, csrfToken);
+  } catch (error) {
+     console.error('Auth check failed:', error);
+  }
+
+  if (authResult.user) {
+      redirect('/dashboard');
+  }
+
+
   return (
     <main className="bg-[url('/backgrounds/auth-bgc.png')] bg-center bg-cover h-[calc(100vh-72px)] flex w-full justify-center items-center flex-col">
       <AuthForm type="login" />
@@ -14,3 +33,5 @@ export default function Home() {
     </main>
   );
 }
+
+export default HomePage;
