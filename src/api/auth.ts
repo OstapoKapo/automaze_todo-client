61a9836a874131c1b04a1858088515739bb08d61
Ostap Prodanyuk <105784216+OstapoKapo@users.noreply.github.com>
2025-09-01 07:31:42 +0300

    Dev (#15)
    * Auth (#10)
    
    * feat: create connect with deploy server for vercel
    
    * feat: create connect with deploy server for vercel (#7)
    
    * feat: create connect with deploy server for vercel
    
    * feat: create connect with deploy server for vercel
    
    * Auth (#13)
    
    * feat: create connect with deploy server for vercel
    
    * feat: create connect with deploy server for vercel
    
    * feat: create connect with deploy server for vercel
    
    * feat: craete connect to server API, closes #12

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,12 @@
-import { RegisterFormProps } from "@/types";
+import { LoginAPIParams, RegisterAPIParams } from "@/types";
 import axiosInstance from "@/utils/restApiConfig";
 
-export const registerUser = async (data: RegisterFormProps) => {
+export const registerUser = async (data: RegisterAPIParams) => {
     const response = await axiosInstance.post('/auth/register', data);
+    return response.data;
+}
+
+export const loginUser = async (data: LoginAPIParams) => {
+    const response = await axiosInstance.post('/auth/login', data);
     return response.data;
 }
